4c78537c6027cfbd2e1c8311f54fed5a9f3e5e85
kdeldycke <kdeldycke@users.noreply.github.com>
2024-01-07 06:00:24 +0000

    [autofix] Format Python

--- .github/metadata.py
+++ .github/metadata.py
@@ -838,7 +838,8 @@ class Metadata:
         # Generate a link to the version of the package published on PyPi.
         pypi_link = ""
         if self.package_name:
-            pypi_link = f"[🐍 Available on PyPi](https://pypi.org/project/{self.package_name}/{version})."
+            pypi_link = f"[🐍 Available on PyPi](https://pypi.org/project/{
+                                                self.package_name}/{version})."
 
         # Assemble the release notes.
         return f"{changes}\n\n{pypi_link}".strip()
