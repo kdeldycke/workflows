b3f60cc58a8e88c34528fbc509865a4075cf8070
Kevin Deldycke <kevin@deldycke.com>
2026-04-01 07:45:44 +0200

    Fix tests

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -458,6 +458,7 @@ expected = {
         "tests/test_platform_keys.py",
         "tests/test_pr_body.py",
         "tests/test_pyproject.py",
+        "tests/test_readme.py",
         "tests/test_release_prep.py",
         "tests/test_release_sync.py",
         "tests/test_renovate.py",
